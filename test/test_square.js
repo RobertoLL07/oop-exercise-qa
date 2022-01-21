5150f99ff3c9a726bf234de58de4175a1638f628
RobertoLL07 <roberto.lobeira@enroutesystems.com>
2022-01-20 22:29:00 -0600

    Change Value on square test

--- test/test_square.js
+++ test/test_square.js
@@ -1,5 +1,5 @@
 let { Square } = require("../classes/main_childs/sub_childs/square")
 
-let square = new Square("pink", true, 5.5);
+let square = new Square("green", true, 5.5);
 console.log(square.getPerimeter());
 console.log(square.getArea());
